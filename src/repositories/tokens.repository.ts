6e98cc24fb3773272c6ff685b5b81f2c2e0a6161
Abdallah <abdallah.moh.q@gmail.com>
2026-02-20 16:45:45 +0200

    Remove hashing the refresh tokens in db (temp as approach wasnt correct)

--- src/repositories/tokens.repository.ts
+++ src/repositories/tokens.repository.ts
@@ -1,24 +1,22 @@
 import { pool } from "../config/db.js";
-import bcrypt from 'bcrypt';
-import { SALT_ROUNDS } from "../services/users.service.js";
 
 export class TokensRepository {
     static async addToken(token: string) {
         const query = "INSERT INTO tokens (refresh_token) VALUES ($1) RETURNING *";
-        const result = await pool.query(query, [await bcrypt.hash(token, SALT_ROUNDS)]);
+        const result = await pool.query(query, [token]);
 
         return result.rows[0];
     }
 
     static async getToken(token: string) {
-        const query = "SELECT id, refresh_token FROM tokens WHERE id=$1";
-        const result = await pool.query(query, [await bcrypt.hash(token, SALT_ROUNDS)]);
+        const query = "SELECT id, refresh_token FROM tokens WHERE refresh_token=$1";
+        const result = await pool.query(query, [token]);
 
         return result.rows[0];
     }
 
     static async deleteToken(token: string) {
         const query = "DELETE FROM tokens WHERE refresh_token=$1";
-        await pool.query(query, [await bcrypt.hash(token, SALT_ROUNDS)]);
+        await pool.query(query, [token]);
     }
 };
