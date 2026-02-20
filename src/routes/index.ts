2af6b02a3ff17eebb946ce1ebbbb63565415a7d6
Abdallah <abdallah.moh.q@gmail.com>
2026-02-20 16:40:10 +0200

    Update how routes are handled

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,10 @@
 import usersRoute from "./users.route.js";
 import tasksRoute from "./tasks.route.js";
+import { Router } from "express";
 
-export { usersRoute, tasksRoute };
+const router = Router();
+
+router.use('/users', usersRoute);
+router.use('/tasks', tasksRoute);
+
+export default router;
